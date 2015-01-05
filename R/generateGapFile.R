1c7ac4695bbe39f7e57b98057f2e1aebe9aa5bd3
Guangchuang Yu <guangchuangyu@gmail.com>
2015-01-05 16:17:29 +0800

    generateGapFile accept summary.csv

--- R/generateGapFile.R
+++ R/generateGapFile.R
@@ -2,7 +2,8 @@
 ##'
 ##' 
 ##' @title generateGapFile
-##' @param out.folder output folder of autoReport, that contains consensus sequences
+##' @param out.folder output folder of autoReport, that contains consensus sequences,
+##'                   or summary.csv from summarize output
 ##' @param ref.folder reference folder
 ##' @param read.fileName list of file names of read file
 ##' @param sample.size sample size
@@ -25,63 +26,34 @@ generateGapFile <- function(out.folder="output", ref.folder="Ref", read.fileName
     reads <- unique(reads)
 
     ref <- list.files(path=ref.folder, pattern=".fas$")
-    
-    cs <- list.files(path=out.folder, pattern=".fas$")
-    cs2 <- paste(out.folder, cs, sep="/") 
 
-    gapfile <- file(gap.file, "w")
-    for (i in 1:length(cs)) {
-        jj <- getAmbiguous.index.base(cs2[i])
-        ii <- jj$index
-        ## x <- toupper(toString(readBStringSet(cs2[i])))
-        ## x <- substring(x, 1:nchar(x), 1:nchar(x))
-        ## ii <- which(! x %in% c("A", "C", "G", "T"))
-        ## if (length(ii) > 0) {
-        ##     ii <- ii[ii > 10 && ii < (length(x)-10)]
-        ## }
-        if (length(ii) == 0) {
-            next
-        }
+    if (file.info(out.folder)$isdir) {
+        cs <- list.files(path=out.folder, pattern=".fas$")
+        cs2 <- paste(out.folder, cs, sep="/")
+    } else {
+        sf <- read.csv(out.folder)
+    }
 
-        ## remove prefix
-        ss <- sub("^[a-zA-Z0-9]+_", "", cs[i])
-        ## remove _mixed if any
-        ss <- sub("_mixed", "", ss)
-        ## remove gene name and .fas
-        ss <- sub("_[A-Z0-9]+.fas$", "", ss)
+    writeGap_ <- function(ss, reads, ref, pp, ii, gapfile, cs) {
         rr <- reads[grep(ss, reads)]
-        if (length(rr) == 0) {
-            warning("file name, ", ss, " not match...")
-            next
-        }
-        ## pp <- gsub(".*_(\\w+).fas", '\\1', cs[i])
-        pp <- gsub(".*_([HNMP][APSB]*\\d*)[_mixed]*\\.fas", '\\1', cs[i])
-        if (pp == "M") {
-            pp <- "MP"
-        } else if (length(grep("H", pp)) > 0) {
-            pp <- "HA"
-        } else if ( length(grep("N\\d", pp)) > 0 ) {
-            ## pp != "NS" && pp != "NP" && length(grep("N", pp)) > 0) {
-            pp <- "NA"
-        }
-        ## rr <- rr[grep(pp, rr)]
         rr <- rr[grep(paste(".*[SRL]+\\d+(", pp, ")\\..*", sep=""), rr)]
-        
         if (length(rr) == 0) {
             warning("gene ", pp, " of ", ss, " not found...")
-            next
+            return(NULL)
         } 
-        ## strain <- gsub(".*_([SRL]+\\d+)[HNMP][APSB]\\d*\\..*", "\\1", rr)
+        
         sg <- get_sid_gn(rr)
         strain <- gsub("([SRL]+\\d+)[HNMP][APSB]*\\d*.*", "\\1", sg)
-        strain <- paste(strain, pp, sep="")
+        strain <- paste(strain, pp, sep="") %>% unique
+        
         ref2 <- ref[grep(strain, ref)]
         if (length(ref2) == 0) {
             warning("reference of ", strain, " not found...")
-            next
-        } 
+            return(NULL)
+        }
         ff <- paste("File:", rr, sep="")
         fr <- paste("Ref:", ref2, sep="")
+
         jj <- paste(ii - 10, ii, sep=",")
         cat("-> ", length(jj), "gap(s) found in", cs[i], "\n")
         if (length(jj) > 1) {
@@ -95,6 +67,65 @@ generateGapFile <- function(out.folder="output", ref.folder="Ref", read.fileName
         writeLines(fr, gapfile)
         writeLines(fg, gapfile)
         writeLines(fn, gapfile)
+    }
+
+    gapfile <- file(gap.file, "w")
+    if (file.info(out.folder)$isdir == FALSE) {
+        for (i in 1:nrow(sf)) {
+            for (j in c("HA", "NA", "PB2", "PB1", "PA", "NP", "MP", "NS")) {
+                entry <- sf[i, j]
+                if (is.null(entry)) {
+                    next
+                }
+                entry <- as.character(entry)
+                if (entry != "" && entry != "full") {
+                    x <- strsplit(entry, "\\|") %>% unlist %>% unique
+                    if (x == "full") {
+                        next
+                    }
+                    ss <- sub("^[a-zA-Z0-9]+_", "", sf[i, "name"])
+                    ii <- strsplit(entry, ",") %>% unlist %>%
+                        strsplit(entry, "\\|") %>% unlist
+                    ii <- ii[ii!="full"]
+                    ii <- sub("\\w$", "", ii) %>% as.numeric
+                    nn <- paste0(as.character(sf[, "name"]), "_", j)
+                    notuse <- writeGap_(ss, reads, ref, j, ii, gapfile, nn)
+                }
+            }
+        }
+    } else {
+        for (i in 1:length(cs)) {
+            jj <- getAmbiguous.index.base(cs2[i])
+            ii <- jj$index
+            ## x <- toupper(toString(readBStringSet(cs2[i])))
+            ## x <- substring(x, 1:nchar(x), 1:nchar(x))
+            ## ii <- which(! x %in% c("A", "C", "G", "T"))
+            ## if (length(ii) > 0) {
+            ##     ii <- ii[ii > 10 && ii < (length(x)-10)]
+            ## }
+            if (length(ii) == 0) {
+                next
+            }
+
+     
+            ## remove prefix
+            ss <- sub("^[a-zA-Z0-9]+_", "", cs[i])
+            ## remove _mixed if any
+            ss <- sub("_mixed", "", ss)
+            ## remove gene name and .fas
+            ss <- sub("_[A-Z0-9]+.fas$", "", ss)
+            ## pp <- gsub(".*_(\\w+).fas", '\\1', cs[i])
+            pp <- gsub(".*_([HNMP][APSB]*\\d*)[_mixed]*\\.fas", '\\1', cs[i])
+            if (pp == "M") {
+                pp <- "MP"
+            } else if (length(grep("H", pp)) > 0) {
+                pp <- "HA"
+            } else if ( length(grep("N\\d", pp)) > 0 ) {
+                ## pp != "NS" && pp != "NP" && length(grep("N", pp)) > 0) {
+                pp <- "NA"
+            }
+            notuse <- writeGap_(ss, reads, ref, pp, ii, gapfile, cs)
+        }
     }
       
     close(gapfile)
