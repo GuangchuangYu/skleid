47377448e5e5a5f8ecb876ebcaced53a45351c16
GuangchuangYu <guangchuangyu@gmail.com>
2015-06-16 14:52:47 +0800

    generate_run_skleid_app

--- R/run_skleid.R
+++ R/run_skleid.R
@@ -13,3 +13,24 @@ run_skleid <- function() {
     options(shiny.launch.browser = TRUE)
     runApp(dir)
 }
+
+
+##' generate desktop shortcut
+##'
+##' 
+##' @title generate_run_skleid_app
+##' @return NULL
+##' @export
+##' @author Guangchuang Yu
+generate_run_skleid_app <- function() {
+    os <- Sys.info()[["sysname"]]
+    if (os != "Windows") {
+        stop("This function only works in Windows Platform...")
+    }
+    
+    outfile <- paste0(file.path(Sys.getenv("USERPROFILE"), "Desktop"), "/run_skleid.bat")
+    out <- file(outfile, "w")
+    
+    writeLines(sub("library/base", "bin/Rscript -e 'skleid::run_skleid()'", system.file(package="base")), out)
+    close(out)
+}
